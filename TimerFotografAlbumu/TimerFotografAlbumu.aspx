5b1fcb55208f4bd6a8c8959845bad6ef109b9445
Oğuzhan Ercan <oguzhanercn7@gmail.com>
2019-09-11 16:53:46 +0300

    TriggerFotoAlbum

---
+++ TimerFotografAlbumu/TimerFotografAlbumu.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="TimerFotografAlbumu.aspx.cs"
+    Inherits="TimerFotografAlbumu" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:ScriptManager ID="ScriptManager1" runat="server" />
+        <center><table width="100%">
+            <tr>
+                <td colspan="2" align="left">
+                    TAKIM RESİMLERİ ALBÜMÜ
+                </td>
+            </tr>
+            <tr>
+                <td width="15%" valign="top">
+                    <asp:ListBox ID="listAlbumler" runat="server" AutoPostBack="True"></asp:ListBox>
+                </td>
+                <td width="85%" valign="top">
+                    <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+                        <ContentTemplate>
+                            <asp:Image ID="imgResim" runat="server" Height="300px" Width="400px" />
+                            <br />
+                            <asp:Button ID="btnOnceki" runat="server" Text="<< Önceki Resim" Width="130px" OnClick="btnOnceki_Click" />
+                            <asp:Button ID="btnSonraki" runat="server" Text="Sonraki Resim >>" Width="130px"
+                                OnClick="btnSonraki_Click" />
+                            <asp:Button ID="btnDurdur" runat="server" Text="Durdur" OnClick="btnDurdur_Click" />
+                            <asp:Button ID="btnBaslat" runat="server" Text="Başlat" OnClick="btnBaslat_Click" />
+                            <asp:Timer ID="Timer1" runat="server" Interval="5000" OnTick="Timer1_Tick">
+                            </asp:Timer>
+                        </ContentTemplate>
+                        <Triggers>
+                            <asp:AsyncPostBackTrigger ControlID="listAlbumler" EventName="SelectedIndexChanged" />
+                        </Triggers>
+                    </asp:UpdatePanel>
+                </td>
+            </tr>
+        </table>
+            </center>
+    </div>
+    </form>
+</body>
+</html>
---
+++ TimerFotografAlbumu/TimerFotografAlbumu.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+public partial class TimerFotografAlbumu : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            string dizin = Server.MapPath("resimler") + "\\";
+            DirectoryInfo di = new DirectoryInfo(dizin);
+            DirectoryInfo[] klasorler = di.GetDirectories();
+            foreach (DirectoryInfo album in klasorler)
+            {
+                listAlbumler.Items.Add(album.Name);
+            }
+
+
+            listAlbumler.SelectedIndex = 0;
+            ResimGoster("ileri");
+
+        }
+    }
+
+    void ResimGoster(string yon)
+    {
+        string albumDizin =
+            Server.MapPath("resimler") + "\\" + listAlbumler.SelectedItem.Text;
+
+        DirectoryInfo di = new DirectoryInfo(albumDizin);
+        FileInfo[] resimler = di.GetFiles("*.jpg");
+
+        
+        int resimNo;
+        if (ViewState["resimNo"] != null)
+        {
+            resimNo = (int)ViewState["resimNo"];
+            if (yon == "ileri")
+            {
+                resimNo++;
+         
+                if (resimNo >= resimler.Length)
+                    resimNo = 0;
+            }
+         
+            else
+            {
+                resimNo--;
+         
+                if (resimNo < 0)
+                    resimNo = resimler.Length - 1;
+            }
+            ViewState["resimNo"] = resimNo;
+        }
+        else
+        {
+            resimNo = 0;
+            ViewState["resimNo"] = resimNo;
+        }
+
+        imgResim.ImageUrl =
+        "resimler\\" + listAlbumler.SelectedItem.Text + "\\" + resimler[resimNo];
+    }
+
+    protected void Timer1_Tick(object sender, EventArgs e)
+    {
+        ResimGoster("ileri");
+    }
+
+    protected void btnOnceki_Click(object sender, EventArgs e)
+    {
+        ResimGoster("geri");
+    }
+
+    protected void btnSonraki_Click(object sender, EventArgs e)
+    {
+        ResimGoster("ileri");
+    }
+
+    protected void btnDurdur_Click(object sender, EventArgs e)
+    {
+        Timer1.Enabled = false;
+    }
+
+    protected void btnBaslat_Click(object sender, EventArgs e)
+    {
+        Timer1.Enabled = true;
+    }
+
+
+}
